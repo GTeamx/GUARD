0865899f4dc4cbb8bcd05933883d42c20e2de4e5
XIII___ <cyjallet@gmail.com>
2022-07-22 18:48:44 +0200

    Added BadPacket I

---
+++ src/main/java/guard/check/checks/player/badpackets/BadPacketI.java
@@ -0,0 +1,25 @@
+package guard.check.checks.player.badpackets;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+import io.github.retrooper.packetevents.packetwrappers.play.in.keepalive.WrappedPacketInKeepAlive;
+
+@GuardCheckInfo(name = "BadPacket I", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+public class BadPacketI extends GuardCheck {
+    private long lastId = -1;
+
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        if (packet.getPacketId() == PacketType.Play.Client.KEEP_ALIVE) {
+            WrappedPacketInKeepAlive p = new WrappedPacketInKeepAlive(packet.getNMSPacket());
+            if (p.getId() == lastId) {
+                fail(packet, "Impossible packet", "KEEP_ALIVE");
+            }
+            lastId = p.getId();
+        }
+    }
+
+}
