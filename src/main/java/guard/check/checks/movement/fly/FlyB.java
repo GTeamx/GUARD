61885036f7dcc5195e32121004e0639022e2e197
XIII___ <cyjallet@gmail.com>
2022-07-22 23:15:16 +0200

    Fixed Fly B

--- src/main/java/guard/check/checks/movement/fly/FlyB.java
+++ src/main/java/guard/check/checks/movement/fly/FlyB.java
@@ -10,12 +10,13 @@ import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 @GuardCheckInfo(name = "Fly B", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 2)
 public class FlyB extends GuardCheck {
     double motionPrediction = 999999999;
+    double pastMotionY;
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
         boolean exempt = isExempt(ExemptType.SLIME, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.LIQUID, ExemptType.GLIDE, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.CLIMBABLE);
-
-        motionPrediction = (lastMotionY - 0.08) * (double)0.98f;
-        if(gp.isInAir() && !exempt && (motionPrediction - lastMotionY > 0.000000000001)) fail(packet, "Predictions unfollowed", "lmy=" + lastMotionY + " py=" + motionPrediction + " result=" + (motionPrediction - lastMotionY));
+        motionPrediction = (pastMotionY - 0.08) * 0.9800000190734863;
+        pastMotionY = motionY;
+        if(gp.isInAir() && !exempt && (motionPrediction - pastMotionY > 0.000000000001)) fail(packet, "Predictions unfollowed", "lmy=" + pastMotionY + " py=" + motionPrediction + " result=" + (motionPrediction - pastMotionY));
         if(!gp.isInAir()) removeBuffer();
     }
 }
