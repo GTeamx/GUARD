a7f66c2338f1cdd1c25364792706dfa5cf61a63f
XIII___ <cyjallet@gmail.com>
2022-07-22 18:47:41 +0200

    Added BadPacket D

---
+++ src/main/java/guard/check/checks/player/badpackets/post/BadPacketD.java
@@ -0,0 +1,21 @@
+package guard.check.checks.player.badpackets.post;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+
+@GuardCheckInfo(name = "BadPacket D", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+public class BadPacketD extends GuardCheck {
+
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        if (packet.getPacketId() == PacketType.Play.Client.CUSTOM_PAYLOAD) {
+            boolean isPost = isPost(packet.getPacketId(), (byte) -100);
+            if (isPost) fail(packet, "Post packet", "CUSTOM_PAYLOAD");
+            if (isPost) packet.setCancelled(true);
+        }
+    }
+
+}
