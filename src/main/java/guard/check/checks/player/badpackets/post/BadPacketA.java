caf9ef17f45b2a4c971de108dd934a7688302ab0
XIII___ <cyjallet@gmail.com>
2022-07-22 23:11:32 +0200

    Fixed BadPacket A

--- src/main/java/guard/check/checks/player/badpackets/post/BadPacketA.java
+++ src/main/java/guard/check/checks/player/badpackets/post/BadPacketA.java
@@ -5,15 +5,12 @@ import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
-import io.github.retrooper.packetevents.packettype.PacketType;
-import io.github.retrooper.packetevents.packetwrappers.play.in.armanimation.WrappedPacketInArmAnimation;
 
 @GuardCheckInfo(name = "BadPacket A", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
 public class BadPacketA extends GuardCheck {
 
     public void onPacket(PacketPlayReceiveEvent packet) {
-        boolean isPost = isPost(packet.getPacketId(), PacketType.Play.Client.ARM_ANIMATION);
-        WrappedPacketInArmAnimation arm = new WrappedPacketInArmAnimation(packet.getNMSPacket());
+        boolean isPost = isPost(packet.getPacketId(), (byte) -100);
         if (isPost) fail(packet, "Post packet", "ARM_ANIMATION");
     }
 
