bb7c4fc60c0192f14521caf4ba0dbc1622171dfd
XIII___ <cyjallet@gmail.com>
2022-07-23 13:14:33 +0200

    Added Step C

---
+++ src/main/java/guard/check/checks/movement/step/StepC.java
@@ -0,0 +1,21 @@
+package guard.check.checks.movement.step;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+
+@GuardCheckInfo(name = "Step C", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 4)
+public class StepC extends GuardCheck {
+
+    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
+        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.SLIME, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.INSIDE_VEHICLE, ExemptType.NEAR_VEHICLE, ExemptType.CLIMBABLE, ExemptType.LIQUID, ExemptType.STAIRS, ExemptType.SLAB, ExemptType.WEB);
+        if(lastMotionY - motionY < 0.01)  {
+            if(!exempt && motionY != 0 && !gp.onLowBlock) fail(packet, "Repeated motion", "lmY=" + lastMotionY + " mY=" + motionY);
+        }
+        if(lastMotionY - motionY > 0.01) removeBuffer();
+    }
+
+}
