99890d0d4957f6d2320b89cfd3e3307f6225ec04
XIII___ <cyjallet@gmail.com>
2022-07-23 13:10:22 +0200

    Improved Fly B

--- src/main/java/guard/check/checks/movement/fly/FlyB.java
+++ src/main/java/guard/check/checks/movement/fly/FlyB.java
@@ -13,7 +13,6 @@ public class FlyB extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
         boolean exempt = isExempt(ExemptType.SLIME, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.LIQUID, ExemptType.GLIDE, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.CLIMBABLE);
-
         motionPrediction = (lastMotionY - 0.08) * (double)0.98f;
         if(gp.isInAir() && !exempt && (motionPrediction - motionY > 0.000000000001)) fail(packet, "Predictions unfollowed", "lmy=" + motionY + " py=" + motionPrediction + " result=" + (motionPrediction - motionY));
         if(!gp.isInAir()) removeBuffer();
