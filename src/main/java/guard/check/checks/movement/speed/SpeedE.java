ef309c8868a572dc0021450154926ff4f6cc06c4
XIII___ <cyjallet@gmail.com>
2022-07-23 13:13:55 +0200

    Added Speed E

---
+++ src/main/java/guard/check/checks/movement/speed/SpeedE.java
@@ -0,0 +1,25 @@
+package guard.check.checks.movement.speed;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+
+@GuardCheckInfo(name = "Speed E", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 1)
+public class SpeedE extends GuardCheck {
+    int airTicks;
+
+    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
+        if(!gp.playerGround) airTicks++;
+        if(gp.playerGround) airTicks = 0;
+        final double prediction = gp.getLastDeltaXZ() * 0.91F + (gp.player.isSprinting() ? 0.026 : 0.02);
+        final double difference = gp.getDeltaXZ() - prediction;
+        final boolean exempt = isExempt(ExemptType.FLYING, ExemptType.NEAR_VEHICLE);
+
+        if (difference > 1e-12 && airTicks > 2 && !exempt) fail(packet, "Predictions unfollowed", "diff=" + difference);
+        else buffer = Math.max(buffer - 1, 0);
+    }
+
+}
