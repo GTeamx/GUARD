fb80b474f2e2036bc96f478dfba5b146d2693b45
XIII___ <cyjallet@gmail.com>
2022-07-23 13:14:24 +0200

    Added Step B

---
+++ src/main/java/guard/check/checks/movement/step/StepB.java
@@ -0,0 +1,19 @@
+package guard.check.checks.movement.step;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+
+@GuardCheckInfo(name = "Step B", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 4)
+public class StepB extends GuardCheck {
+
+    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
+        boolean exempt = isExempt(ExemptType.PLACE, ExemptType.STAIRS, ExemptType.GLIDE, ExemptType.SLAB, ExemptType.SLIME, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.TELEPORT, ExemptType.LIQUID, ExemptType.PLACE);
+        if(motionY > 0.117600002289 && !gp.onLowBlock && !exempt) fail(packet, "Added ticks", "mY=" + motionY);
+        if(motionY <= 0.117600002289 && !gp.isInAir() || gp.playerGround) removeBuffer();
+    }
+
+}
