5d6b46a8eeec427bc39ade8cd57e18ec695b5374
XIII___ <cyjallet@gmail.com>
2022-07-30 00:04:59 +0200

    Added KillAura B

---
+++ src/main/java/guard/check/checks/combat/killaura/KillAuraB.java
@@ -0,0 +1,13 @@
+package guard.check.checks.combat.killaura;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+
+@GuardCheckInfo(name = "KillAura B", category = GuardCategory.Combat, state = GuardCheckState.Coding, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+public class KillAuraB extends GuardCheck {
+
+    // MultiAura check
+
+}
