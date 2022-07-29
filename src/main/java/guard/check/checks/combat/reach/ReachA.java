a633a189fb5898cc2b6cdaa1d9da5db81d0334b6
XIII___ <cyjallet@gmail.com>
2022-07-30 00:05:31 +0200

    Added Reach A (EMPTY)

---
+++ src/main/java/guard/check/checks/combat/reach/ReachA.java
@@ -0,0 +1,10 @@
+package guard.check.checks.combat.reach;
+
+import guard.check.GuardCategory;
+import guard.check.GuardCheck;
+import guard.check.GuardCheckInfo;
+import guard.check.GuardCheckState;
+
+@GuardCheckInfo(name = "Reach A", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+public class ReachA extends GuardCheck {
+}
