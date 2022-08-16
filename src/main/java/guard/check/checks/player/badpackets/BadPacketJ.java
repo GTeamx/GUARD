f70157df733d94abc40cd5ad82fef8a59f80fa93
Vagdedes2 <ickbinbesser12@gmail.com>
2022-08-16 23:29:25 +0200

    BadPacketJ improved and added a small Buffer.

--- src/main/java/guard/check/checks/player/badpackets/BadPacketJ.java
+++ src/main/java/guard/check/checks/player/badpackets/BadPacketJ.java
@@ -10,14 +10,14 @@ import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.play.in.abilities.WrappedPacketInAbilities;
 import org.bukkit.GameMode;
 
-@GuardCheckInfo(name = "BadPacket J", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+@GuardCheckInfo(name = "BadPacket J", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.05, maxBuffer = 1)
 public class BadPacketJ extends GuardCheck {
 
     public void onPacket(PacketPlayReceiveEvent packet) {
         if(packet.getPacketId() == PacketType.Play.Client.ABILITIES) {
             WrappedPacketInAbilities wrapper = new WrappedPacketInAbilities(packet.getNMSPacket());
             boolean exempt = isExempt(ExemptType.FLYING);
-            if((wrapper.isFlying() && !gp.getPlayer().isFlying()) && !exempt && gp.player.getGameMode() != GameMode.CREATIVE && gp.player.getGameMode() != GameMode.SPECTATOR) fail(packet, "Impossible packet", "ABILITIES");
+            if(((wrapper.isFlying() && !gp.getPlayer().isFlying() && !gp.getPlayer().getAllowFlight()) || (!gp.getPlayer().getAllowFlight() && wrapper.isFlightAllowed().isPresent() ? wrapper.isFlightAllowed().get() : true)) && !exempt && gp.player.getGameMode() != GameMode.CREATIVE && gp.player.getGameMode() != GameMode.SPECTATOR) fail(packet, "Impossible packet", "ABILITIES");
         }
     }
 
