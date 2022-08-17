5bf1002f15c770c074e6d3ce2eccb6f9c0d54e62
XIII___ <cyjallet@gmail.com>
2022-08-17 16:45:32 +0200

    Improved Aim B

--- src/main/java/guard/check/checks/combat/aim/AimB.java
+++ src/main/java/guard/check/checks/combat/aim/AimB.java
@@ -7,24 +7,19 @@ import guard.check.GuardCheckState;
 import guard.utils.MathUtils;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Aim B", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.05, maxBuffer = 2)
+@GuardCheckInfo(name = "Aim B", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.05, maxBuffer = 2.25)
 public class AimB extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
 
         if(deltaPitch > 1) {
 
-            final double expendedPitch = deltaPitch * MathUtils.EXPANDER;
-            final double lastExpendedPitch = lastDeltaPitch * MathUtils.EXPANDER;
-
-            final double gcdPitch = MathUtils.getGcd(expendedPitch, lastExpendedPitch);
-            final double dividedPitch = gcdPitch / MathUtils.EXPANDER;
-
-            final double dfPitch = Math.abs((double) gp.getPlayer().getLocation().getPitch() % dividedPitch);
+            final double gcdPitch = (MathUtils.getGcd(deltaPitch * MathUtils.EXPANDER, lastDeltaPitch * MathUtils.EXPANDER)) / MathUtils.EXPANDER;
+            final double dfPitch = Math.abs((double) gp.getPlayer().getLocation().getPitch() % gcdPitch);
 
             debug("dfP=" + dfPitch + " b=" + buffer);
 
-            if(dfPitch < 0.00008) fail(null, "GCD Flaw", "dfP=" + dfPitch);
+            if(dfPitch < 1.2E-5) fail(null, "GCD Flaw", "dfP=" + dfPitch);
             else removeBuffer();
         }
 
