1b76908dc49cb1d9c51579995999295a062fae65
XIII___ <cyjallet@gmail.com>
2022-08-18 13:43:15 +0200

    Recoded Fly F

--- src/main/java/guard/check/checks/movement/fly/FlyF.java
+++ src/main/java/guard/check/checks/movement/fly/FlyF.java
@@ -7,19 +7,15 @@ import guard.check.GuardCheckState;
 import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Fly F", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+@GuardCheckInfo(name = "Fly F", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 2)
 public class FlyF extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        boolean exempt = isExempt(ExemptType.TELEPORT, ExemptType.SLIME, ExemptType.LIQUID, ExemptType.WEB, ExemptType.GROUND, ExemptType.JOINED, ExemptType.INSIDE_VEHICLE, ExemptType.FLYING);
-        boolean step = mathOnGround(motionY) && mathOnGround(gp.from.getY());
-        double predictionMotionY = (lastMotionY - 0.08D) * (double) 0.98F;
-        boolean jumped = !gp.playerGround && gp.lastPlayerGround;
-        if(jumped) {
-            predictionMotionY = 0.42f;
-        }
-        debug("predicted=" + predictionMotionY + " step=" + step + " diff=" + Math.abs(motionY - predictionMotionY) + " jumped=" + jumped);
-        if(!exempt && !step && Math.abs(motionY - predictionMotionY) > 1.0 && (System.currentTimeMillis() - gp.lastHurt > 300)) fail(packet, "Teleported downwards", "mY=" + Math.abs(motionY - predictionMotionY)); else removeBuffer();
+        final boolean exempt = isExempt(ExemptType.FLYING, ExemptType.FULL_LIQUID, ExemptType.CLIMBABLE, ExemptType.TELEPORT, ExemptType.GLIDE, ExemptType.NEAR_VEHICLE, ExemptType.SLIME, ExemptType.PLACE);
+        maxBuffer = isExempt(ExemptType.VELOCITY) ? 2 : 4;
+        final double predictedMotionY = (lastMotionY - 0.08D) * (double)0.98F;
+        if((Math.abs(motionY - predictedMotionY) > 0.00000000001) && !gp.playerGround && !exempt) fail(packet, "Predictions unfollowed", "pred=" + predictedMotionY + " mY=" + motionY);
+        else if(gp.playerGround && gp.serverGround) removeBuffer();
     }
 
 }
