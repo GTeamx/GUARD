b63090c2a35691843f80f90e85b1f01e5f87dcde
XIII___ <cyjallet@gmail.com>
2022-08-18 13:42:49 +0200

    Recoded Fly E

--- src/main/java/guard/check/checks/movement/fly/FlyE.java
+++ src/main/java/guard/check/checks/movement/fly/FlyE.java
@@ -8,12 +8,15 @@ import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import org.bukkit.potion.PotionEffectType;
 
-@GuardCheckInfo(name = "Fly E", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
+@GuardCheckInfo(name = "Fly E", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 1)
 public class FlyE extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.SLIME, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.VELOCITY, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.NEAR_VEHICLE);
-        if(motionY > .6 + (gp.player.hasPotionEffect(PotionEffectType.JUMP) ? ((gp.getPotionEffectAmplifier(PotionEffectType.JUMP) * 0.1) + 1) : 0) && !exempt) fail(packet, "Teleported upwards", "mY=" + motionY); else removeBuffer();
+        final boolean exempt = isExempt(ExemptType.FLYING, ExemptType.FULL_LIQUID, ExemptType.CLIMBABLE, ExemptType.TELEPORT, ExemptType.GLIDE, ExemptType.NEAR_VEHICLE, ExemptType.PLACE);
+        maxBuffer = isExempt(ExemptType.VELOCITY) ? 1 : 3;
+        final double predictedMotionY = (lastMotionY - 0.08D) * (double)0.98F;
+        if((motionY - predictedMotionY > 0.0000000001) && !gp.playerGround && !exempt) fail(packet, "Predictions unfollowed", "pred=" + predictedMotionY + " mY=" + motionY);
+        else if(gp.playerGround && gp.serverGround) removeBuffer();
     }
 
 }
