6ae9e8bc7bb2935613c30ffcad4e1bc1aa691f2e
XIII___ <cyjallet@gmail.com>
2022-08-19 10:51:15 +0200

    Recoded Aim D

--- src/main/java/guard/check/checks/combat/aim/AimD.java
+++ src/main/java/guard/check/checks/combat/aim/AimD.java
@@ -4,35 +4,24 @@ import guard.check.GuardCategory;
 import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
+import guard.utils.MathUtils;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
-import io.github.retrooper.packetevents.packettype.PacketType;
-import io.github.retrooper.packetevents.packetwrappers.play.out.position.WrappedPacketOutPosition;
 
-@GuardCheckInfo(name = "Aim D", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 4)
+@GuardCheckInfo(name = "Aim D", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.01, maxBuffer = 2)
 public class AimD extends GuardCheck {
 
-    float currentPitch, previousPitch, currentYaw, previousYaw;
-
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        if(deltaPitch > 0) {
 
-            currentPitch = gp.getPlayer().getLocation().getPitch();
+        if(deltaPitch != 0) {
 
-            if(deltaPitch == lastDeltaPitch) fail(packet, "Repeated Rotation", "PITCH");
-            else removeBuffer();
+            double cPitch = deltaPitch * 16777216;
+            double cLastPitch = lastDeltaPitch * 16777216;
+            final double GCD = MathUtils.getGcd(cPitch, cLastPitch);
+            final double moduloGCD = GCD % gp.getPlayer().getLocation().getPitch();
 
-            previousPitch = gp.getPlayer().getLocation().getPitch();
+            debug("moduloGCD=" + moduloGCD);
 
-        }
-
-        if(deltaYaw > 0) {
-
-            currentYaw = gp.getPlayer().getLocation().getYaw();
-
-            if(deltaYaw == lastDeltaYaw) fail(packet, "Repeated Rotation", "YAW");
-            else removeBuffer();
-
-            previousYaw = gp.getPlayer().getLocation().getYaw();
+            if(String.valueOf(moduloGCD).substring(1).equalsIgnoreCase(".0")) fail(packet, "Perfect Rotation", "mGCD=" + moduloGCD);
 
         }
     }
