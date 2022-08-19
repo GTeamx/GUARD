7dd96e94650847fa52539d2e5720e33f7f02002e
XIII___ <cyjallet@gmail.com>
2022-08-19 12:13:53 +0200

    Improved Fly E

--- src/main/java/guard/check/checks/movement/fly/FlyE.java
+++ src/main/java/guard/check/checks/movement/fly/FlyE.java
@@ -8,7 +8,7 @@ import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import org.bukkit.potion.PotionEffectType;
 
-@GuardCheckInfo(name = "Fly E", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 1)
+    @GuardCheckInfo(name = "Fly E", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 2)
 public class FlyE extends GuardCheck {
 
     boolean wasVelocity;
@@ -26,7 +26,7 @@ public class FlyE extends GuardCheck {
         if(gp.playerGround) wasVelocity = false;
         if(isExempt(ExemptType.VELOCITY)) wasVelocity = true;
         final double predictedMotionY = (lastMotionY - 0.08D) * (double)0.98F;
-        maxBuffer = (wasVelocity ? tempBuffer + 3 : tempBuffer);
+        maxBuffer = (wasVelocity ? tempBuffer + 1 : tempBuffer);
         if((motionY - predictedMotionY > 0.0000000001) && !gp.playerGround && !exempt) fail(packet, "Predictions unfollowed", "pred=" + predictedMotionY + " mY=" + motionY);
         else if(gp.playerGround && gp.serverGround) removeBuffer();
     }
