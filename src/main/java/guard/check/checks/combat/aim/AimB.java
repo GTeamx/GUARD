ab005d0953b012856ad03b087697faf7140c1480
XIII___ <cyjallet@gmail.com>
2022-08-19 10:50:30 +0200

    Recoded Aim B

--- src/main/java/guard/check/checks/combat/aim/AimB.java
+++ src/main/java/guard/check/checks/combat/aim/AimB.java
@@ -7,19 +7,21 @@ import guard.check.GuardCheckState;
 import guard.utils.MathUtils;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Aim B", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.15, maxBuffer = 2.25)
+@GuardCheckInfo(name = "Aim B", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
 public class AimB extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
 
-        if(deltaPitch > 1) {
+        if(deltaPitch != 0) {
 
-            final double gcdPitch = (MathUtils.getGcd(deltaPitch * MathUtils.EXPANDER, lastDeltaPitch * MathUtils.EXPANDER)) / MathUtils.EXPANDER;
-            final double dfPitch = Math.abs((double) gp.getPlayer().getLocation().getPitch() % gcdPitch);
+            double cPitch = deltaPitch * 16777216;
+            double cLastPitch = lastDeltaPitch * 16777216;
+            final double GCD = MathUtils.getGcd(cPitch, cLastPitch);
+            final double moduloGCD = GCD % lastDeltaPitch;
 
-            debug("dfP=" + dfPitch + " b=" + buffer);
+            debug("moduloGCD=" + moduloGCD + " b=" + buffer);
 
-            if(dfPitch < 1.2E-5) fail(null, "GCD Flaw", "dfP=" + dfPitch);
+            if(Math.abs(moduloGCD) > 20) fail(null, "GCD Flaw", "moduloGCD=" + moduloGCD);
             else removeBuffer();
         }
 
