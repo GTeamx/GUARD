477cf01f51c7f8bf1d488984aa79c30edb35b668
XIII___ <cyjallet@gmail.com>
2022-08-19 09:57:18 +0200

    Improved AutoBlock A

--- src/main/java/guard/check/checks/combat/autoblock/AutoBlockA.java
+++ src/main/java/guard/check/checks/combat/autoblock/AutoBlockA.java
@@ -11,11 +11,14 @@ import io.github.retrooper.packetevents.packetwrappers.play.in.useentity.Wrapped
 @GuardCheckInfo(name = "AutoBlock A", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 0, removeBuffer = 0, maxBuffer = 0)
 public class AutoBlockA extends GuardCheck {
 
+    double lastUse;
+
     @Override
     public void onPacket(PacketPlayReceiveEvent packet) {
         if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
             WrappedPacketInUseEntity wrapper = new WrappedPacketInUseEntity(packet.getNMSPacket());
-            if((System.currentTimeMillis() - gp.lastBlockPlaced <= 1) && wrapper.getAction() == WrappedPacketInUseEntity.EntityUseAction.ATTACK) fail(packet, "Invalid packet order", "plT=" + (System.currentTimeMillis() - gp.lastBlockPlaced));
-        }
+            debug("delay=" + (System.currentTimeMillis() - lastUse ));
+            if((System.currentTimeMillis() - lastUse < 86) && wrapper.getAction() == WrappedPacketInUseEntity.EntityUseAction.ATTACK) fail(packet, "Invalid packet order", "plT=" + (System.currentTimeMillis() - gp.lastBlockPlaced));
+        } else if(packet.getPacketId() == PacketType.Play.Client.BLOCK_PLACE) lastUse = System.currentTimeMillis();
     }
 }
