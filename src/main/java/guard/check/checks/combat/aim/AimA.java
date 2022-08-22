2aabab48941d6bf0dc307b997d3f27bd063f7b42
XIII___ <cyjallet@gmail.com>
2022-08-22 22:55:53 +0200

    Recoded Aim A

--- src/main/java/guard/check/checks/combat/aim/AimA.java
+++ src/main/java/guard/check/checks/combat/aim/AimA.java
@@ -7,19 +7,16 @@ import guard.check.GuardCheckState;
 import guard.utils.MathUtils;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Aim A", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 6)
+@GuardCheckInfo(name = "Aim A", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 9)
 public class AimA extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
 
         if(deltaPitch != 0) {
+            final double GCD = MathUtils.getGcd(deltaPitch * MathUtils.EXPANDER, lastDeltaPitch * MathUtils.EXPANDER);
 
-            double cPitch = deltaPitch * 16777216;
-            double cLastPitch = lastDeltaPitch * 16777216;
-            final double GCD = MathUtils.getGcd(cPitch, cLastPitch);
-
-            if(GCD > 0 && GCD < 300000 && !gp.isCinematic) fail(null, "Invalid GCD", "gcd=" + GCD);
-            if(GCD > 300000 && !gp.isCinematic) removeBuffer();
+            if(GCD < 131072 && !gp.isCinematic && deltaPitch > 0.5 && deltaPitch < 20) fail(null, "Invalid GCD", "gcd=" + GCD);
+            if(GCD > 131072 && !gp.isCinematic) removeBuffer();
 
         }
 
