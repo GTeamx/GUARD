19313f052357caf7bfec12f5160a84609be332ed
XIII___ <cyjallet@gmail.com>
2022-08-22 22:56:37 +0200

    Removed Aim C

--- src/main/java/guard/check/checks/combat/aim/AimC.java
+++ src/main/java/guard/check/checks/combat/aim/AimC.java
@@ -9,21 +9,16 @@ import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
 import org.bukkit.Bukkit;
 
-@GuardCheckInfo(name = "Aim C", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.25, maxBuffer = 3.25)
+@GuardCheckInfo(name = "Aim C", category = GuardCategory.Combat, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 4)
 public class AimC extends GuardCheck {
 
     public void onPacket(PacketPlayReceiveEvent packet) {
         if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
 
-            if(gp.deltaPitch != 0 && gp.deltaYaw != 0) {
-
-                final double ctP = (MathUtils.getGcd(gp.deltaPitch * MathUtils.EXPANDER, gp.lastDeltaPitch * MathUtils.EXPANDER)) / MathUtils.EXPANDER;
-                final double ctY = (MathUtils.getGcd(gp.deltaYaw * MathUtils.EXPANDER, gp.lastDeltaYaw * MathUtils.EXPANDER)) / MathUtils.EXPANDER;
-                final double fmP = Math.abs(Math.floor(gp.deltaPitch / ctP % gp.lastDeltaPitch / ctP) - (gp.deltaYaw / ctY % gp.lastDeltaYaw / ctY));
-                if (Math.abs(fmP / (gp.deltaPitch * MathUtils.EXPANDER)) < 7E-10) fail(null, "Impossible Rotation", "fMp=" + Math.abs(fmP / (gp.deltaPitch * MathUtils.EXPANDER)));
+            if(gp.deltaYaw != 0) {
+                if (gp.deltaPitch == gp.lastDeltaPitch) fail(packet, "Repeated deltaPitch", "rpP=" + gp.deltaPitch);
                 else removeBuffer();
             }
-
         }
     }
 }
