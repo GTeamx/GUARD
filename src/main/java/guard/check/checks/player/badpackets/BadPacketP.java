b5139dd1283a039161613ee19860170f28d34c03
XIII___ <cyjallet@gmail.com>
2022-08-23 14:13:55 +0200

    Improved BadPacket P

--- src/main/java/guard/check/checks/player/badpackets/BadPacketP.java
+++ src/main/java/guard/check/checks/player/badpackets/BadPacketP.java
@@ -10,36 +10,19 @@ import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
 import io.github.retrooper.packetevents.packetwrappers.play.in.useentity.WrappedPacketInUseEntity;
 
-@GuardCheckInfo(name = "BadPacket P", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 1)
+@GuardCheckInfo(name = "BadPacket P", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.5, maxBuffer = 1)
 public class BadPacketP extends GuardCheck {
+
     boolean cSwing;
-    long lastPacket;
-    long lastId;
-    int ping;
 
     public void onPacket(PacketPlayReceiveEvent packet) {
+        if (packet.getPacketId() == PacketType.Play.Client.ARM_ANIMATION) cSwing = true;
         if (packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
             WrappedPacketInUseEntity wrapper = new WrappedPacketInUseEntity(packet.getNMSPacket());
-            debug("cSwing=" + cSwing + " transactionPing=" + gp.transactionPing + " ping=" + ping);
-            if (wrapper.getAction() == WrappedPacketInUseEntity.EntityUseAction.ATTACK && !cSwing && gp.transactionPing >= ping - 10) fail(packet, "Missing packet", "ARM_ANIMATION ? USE_ENTITY");
+            debug("cSwing=" + cSwing);
+            if (wrapper.getAction() == WrappedPacketInUseEntity.EntityUseAction.ATTACK && !cSwing) fail(packet, "Missing packet", "ARM_ANIMATION ? USE_ENTITY");
             else removeBuffer();
             cSwing = false;
         }
-        if (packet.getPacketId() == PacketType.Play.Client.ARM_ANIMATION) cSwing = true;
-    }
-
-
-
-    public void onTransaction(TransactionPacketClient packet, boolean found) {
-        if (lastId == packet.getId()) {
-            ping = (int) (packet.getTimeStamp() - lastPacket);
-        }
-    }
-
-
-
-    public void onTransactionSend(TransactionPacketServer packet) {
-        lastPacket = packet.getTimeStamp();
-        lastId = packet.getId();
     }
 }
