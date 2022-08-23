666ade1c5550ef37b4e83c26c5baa3b6281dee61
XIII___ <cyjallet@gmail.com>
2022-08-23 14:17:02 +0200

    Tried to recode PingSpoof A

--- src/main/java/guard/check/checks/player/pingspoof/PingSpoofA.java
+++ src/main/java/guard/check/checks/player/pingspoof/PingSpoofA.java
@@ -4,11 +4,13 @@ import guard.check.GuardCategory;
 import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
 import guard.utils.SampleList;
 import guard.utils.packet.TransactionPacketClient;
 import guard.utils.packet.TransactionPacketServer;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
+import org.bukkit.Bukkit;
 
 @GuardCheckInfo(name = "PingSpoof A", category = GuardCategory.Player, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 1)
 public class PingSpoofA extends GuardCheck {
@@ -32,6 +34,12 @@ public class PingSpoofA extends GuardCheck {
     public void onTransaction(TransactionPacketClient packet, boolean found) {
        if(id == packet.getId() && checked) {
            transactionPing.add((int) (packet.getTimeStamp() - lastTransaction));
+
+           final boolean exempt = isExempt(ExemptType.TPS, ExemptType.CHUNK, ExemptType.TELEPORT, ExemptType.JOINED);
+           if(!exempt && gp.ping > transactionPing.getAverageInt(transactionPing) && Math.abs(gp.ping - transactionPing.getAverageInt(transactionPing)) > 50) Bukkit.broadcastMessage("(A) " + Math.abs(gp.ping - transactionPing.getAverageInt(transactionPing)));
+           if(!exempt && transactionPing.getAverageInt(transactionPing) > gp.ping && Math.abs(transactionPing.getAverageInt(transactionPing) - gp.ping) > 50) Bukkit.broadcastMessage("(B) " + Math.abs(gp.ping - transactionPing.getAverageInt(transactionPing)));
+           debug("ping=" + gp.ping + " tP=" + transactionPing + " re=" + Math.abs(transactionPing.getAverageInt(transactionPing) - gp.ping));
+           /*transactionPing.add((int) (packet.getTimeStamp() - lastTransaction));
            if(transactionPing.isCollected()) {
                int diff = gp.ping - transactionPing.getAverageInt(transactionPing);
                 if(diff > 70 && wasKeepalive) {
@@ -44,7 +52,7 @@ public class PingSpoofA extends GuardCheck {
                 }
                debug("transactionPing=" + transactionPing.getAverageInt(transactionPing) + " keepalivePing=" + gp.ping + " buffer=" + buffer);
            }
-           checked = false;
+           checked = false;*/
 
        }
 
