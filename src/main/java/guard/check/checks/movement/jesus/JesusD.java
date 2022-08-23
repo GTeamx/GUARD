09c905cdf968d6aa10cb94f3bb9d26752fa9c533
XIII___ <cyjallet@gmail.com>
2022-08-23 14:15:48 +0200

    Improved Jesus D

--- src/main/java/guard/check/checks/movement/jesus/JesusD.java
+++ src/main/java/guard/check/checks/movement/jesus/JesusD.java
@@ -4,6 +4,7 @@ import guard.check.GuardCategory;
 import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.utils.player.ClientVersion;
@@ -18,7 +19,7 @@ public class JesusD extends GuardCheck {
                 exempt = true;
             }
         }
-        if(gp.isInFullLiquid && gp.blockAboveWater) {
+        if(gp.isInFullLiquid && gp.blockAboveWater && !isExempt(ExemptType.FLYING)) {
             if(motionY - lastMotionY <= 0.0025 && motionY - lastMotionY >= 0 && motionY < 0.09 && motionY > -0.1 && !exempt && !String.valueOf(motionY).startsWith("0.03999999") && !String.valueOf(motionY).startsWith("0.040000000")) {
                 fail(null, "Same DeltaY", "motionY=" + motionY);
                 debug("" +( motionY - lastMotionY));
