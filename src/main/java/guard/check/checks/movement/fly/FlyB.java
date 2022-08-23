c2b7dd9fe354241bbc3d0e8fbaa7965ca6ed8b9a
XIII___ <cyjallet@gmail.com>
2022-08-23 14:14:46 +0200

    Improved Fly B

--- src/main/java/guard/check/checks/movement/fly/FlyB.java
+++ src/main/java/guard/check/checks/movement/fly/FlyB.java
@@ -8,12 +8,12 @@ import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.PacketEvents;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Fly B", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 2)
+@GuardCheckInfo(name = "Fly B", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0, maxBuffer = 4)
 public class FlyB extends GuardCheck {
     double motionPrediction = 999999999;
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        boolean exempt = isExempt(ExemptType.SLIME, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.FULL_LIQUID, ExemptType.GLIDE, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.CLIMBABLE, ExemptType.VELOCITY, ExemptType.PLACE, ExemptType.TELEPORT);
+        boolean exempt = isExempt(ExemptType.SLIME, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.FULL_LIQUID, ExemptType.GLIDE, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.CLIMBABLE, ExemptType.VELOCITY, ExemptType.PLACE, ExemptType.TELEPORT, ExemptType.LIQUID);
         motionPrediction = (lastMotionY - 0.08) * (double)0.98f;
         boolean isBedrock = PacketEvents.get().getPlayerUtils().isGeyserPlayer(gp.player) || gp.player.getName().contains(".");
         debug("result=" + (motionPrediction - motionY) + "/" + 0.000000000001 +  " pred=" + motionPrediction + " mY=" + motionY);
