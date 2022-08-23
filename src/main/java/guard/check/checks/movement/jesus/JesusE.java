ca671c13a394e64482fd8c6b643d0c1724d6ed5b
XIII___ <cyjallet@gmail.com>
2022-08-23 14:16:04 +0200

    Improved Jesus E

--- src/main/java/guard/check/checks/movement/jesus/JesusE.java
+++ src/main/java/guard/check/checks/movement/jesus/JesusE.java
@@ -4,6 +4,7 @@ import guard.check.GuardCategory;
 import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
 @GuardCheckInfo(name = "Jesus E", category = GuardCategory.Movement, state = GuardCheckState.Coding, addBuffer = 1, removeBuffer = 1, maxBuffer = 3)
@@ -11,8 +12,8 @@ public class JesusE extends GuardCheck {
 
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        if(gp.aboveLiquid || gp.isInFullLiquid ) {
-            if(motionY > 0.2 && gp.blockAboveWater) {
+        if(gp.aboveLiquid || gp.isInFullLiquid) {
+            if(motionY > 0.2 && gp.blockAboveWater && !isExempt(ExemptType.FLYING, ExemptType.SWIMMING)) {
                 fail(null, "Invalid DeltaY", "motionY=" + motionY);
 
             }else removeBuffer();
