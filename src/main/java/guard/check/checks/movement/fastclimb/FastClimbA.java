c1aedea105382e09640f9bd5be81552c9494a4ce
XIII___ <cyjallet@gmail.com>
2022-08-23 14:18:37 +0200

    Improved FastClimb A

--- src/main/java/guard/check/checks/movement/fastclimb/FastClimbA.java
+++ src/main/java/guard/check/checks/movement/fastclimb/FastClimbA.java
@@ -11,7 +11,7 @@ import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 public class FastClimbA extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        boolean exempt = isExempt(ExemptType.PLACE, ExemptType.STAIRS, ExemptType.GLIDE, ExemptType.SLAB, ExemptType.SLIME, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.TELEPORT, ExemptType.LIQUID, ExemptType.PLACE);
+        boolean exempt = isExempt(ExemptType.PLACE, ExemptType.STAIRS, ExemptType.GLIDE, ExemptType.SLAB, ExemptType.SLIME, ExemptType.FLYING, ExemptType.NEAR_VEHICLE, ExemptType.INSIDE_VEHICLE, ExemptType.TELEPORT, ExemptType.LIQUID, ExemptType.PLACE, ExemptType.FULL_LIQUID);
         if(motionY > 0.117600002289 && !gp.onLowBlock && !exempt && isExempt(ExemptType.CLIMBABLE)) fail(packet, "Climbing too fast", "mY=" + motionY); else removeBuffer();
     }
 
