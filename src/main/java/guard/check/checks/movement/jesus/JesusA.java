393d10d6c91bd575bd7b36287ca6e77dc7fd09b4
XIII___ <cyjallet@gmail.com>
2022-08-24 16:41:39 +0200

    Improved Jesus A

--- src/main/java/guard/check/checks/movement/jesus/JesusA.java
+++ src/main/java/guard/check/checks/movement/jesus/JesusA.java
@@ -4,13 +4,14 @@ import guard.check.GuardCategory;
 import guard.check.GuardCheck;
 import guard.check.GuardCheckInfo;
 import guard.check.GuardCheckState;
+import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
 @GuardCheckInfo(name = "Jesus A", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 1, maxBuffer = 1)
 public class JesusA extends GuardCheck {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
-        if(gp.aboveLiquid && !gp.isInFullLiquid) {
+        if(gp.aboveLiquid && !gp.isInFullLiquid && !isExempt(ExemptType.TELEPORT)) {
             fail(null, "Walked On Water", "true false");
             //debug("HAKE?");
         } else removeBuffer();
