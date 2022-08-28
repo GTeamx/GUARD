dff8e60f5c44a118425816a9816f86dc3ee557c1
XIII___ <cyjallet@gmail.com>
2022-08-28 22:57:24 +0200

    kinda improved speed f ig?

--- src/main/java/guard/check/checks/movement/speed/SpeedF.java
+++ src/main/java/guard/check/checks/movement/speed/SpeedF.java
@@ -7,14 +7,14 @@ import guard.check.GuardCheckState;
 import guard.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 
-@GuardCheckInfo(name = "Speed F", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.10, maxBuffer = 1)
+@GuardCheckInfo(name = "Speed F", category = GuardCategory.Movement, state = GuardCheckState.Testing, addBuffer = 1, removeBuffer = 0.20, maxBuffer = 1)
 public class SpeedF extends GuardCheck {
 
     @Override
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastMotionX, double lastMotionY, double lastMotionZ, float deltaYaw, float deltaPitch, float lastDeltaYaw, float lastDeltaPitch) {
         final boolean exempt = isExempt(ExemptType.TELEPORT, ExemptType.CLIMBABLE, ExemptType.STAIRS, ExemptType.SLAB);
         final double predictedMotion = Math.sqrt(Math.abs(motionY - lastMotionY));
-        if(!gp.onLowBlock && !exempt && (motionY - predictedMotion < -0.6456 || motionY - predictedMotion > 0.0386) && !gp.inAir && gp.onSolidGround) fail(packet, "YPort Motion"," res=" + (motionY - predictedMotion));
+        if(!gp.onLowBlock && !exempt && (motionY - predictedMotion < -0.6456 || (motionY - predictedMotion > 0 && motionY - predictedMotion != 0.0386)) && !gp.inAir && gp.onSolidGround) fail(packet, "YPort Motion"," res=" + (motionY - predictedMotion));
         else removeBuffer();
         if(!gp.inAir && motionY > -0.38 && motionY <= 0.42) debug("res=" + (motionY - predictedMotion) + " my=" + motionY + " g=" + gp.playerGround + " sG=" + gp.onSolidGround + " sG=" + gp.serverGround + " iA=" + gp.inAir);
 
